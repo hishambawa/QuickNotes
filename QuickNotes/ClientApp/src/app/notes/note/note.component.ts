921a881f989525ac847653d5353084799ae63a3b
Hisham Bawa <hishambawa@icloud.com>
2022-06-14 11:15:44 +0530

    Added angular mainpage with notes component

---
+++ QuickNotes/ClientApp/src/app/notes/note/note.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note',
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.css']
+})
+export class NoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  deleteNote(): void {
+    alert("deleting note");
+  }
+
+}
---
+++ QuickNotes/ClientApp/src/app/notes/notes.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NoteComponent } from './note/note.component';
+
+
+
+@NgModule({
+  declarations: [
+    NoteComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [NoteComponent]
+})
+export class NotesModule { }
